26fcf610bb4b6cdf487bee4ee99604c69ae431fe
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-11-22 12:23:01 +0800

    fix json parse issue

--- comm/index.js
+++ comm/index.js
@@ -1,12 +1,21 @@
 const { get, } = require('lodash')
 const debug = require('debug')('READR:api:comm')
 
+const isValidJSONString = str => {
+  try {
+      JSON.parse(str)
+  } catch (e) {
+      return false
+  }
+  return true
+}
 const handlerError = (err, res) => {
   debug('err:')
   debug(err)
+  const text = get(res, 'text') || get(err, 'message', '{}')
   return {
     status: (typeof(get(res, 'status')) === 'number' && get(res, 'status')) || get(err, 'status') || 500,
-    text: get(res, 'text') || get(err, 'message', ''),
+    text: isValidJSONString(text) ? text : `{}`,
   }
 }
 module.exports = {
