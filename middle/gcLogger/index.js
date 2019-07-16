25707644faaddabe83791d8321de93466628f4ba
Noah Tan <tempo0829@gmail.com>
2019-07-16 14:33:54 +0800

    upgrade google packages version

--- middle/gcLogger/index.js
+++ middle/gcLogger/index.js
@@ -3,25 +3,22 @@ const debug = require('debug')('READR-API:api:gcLogger')
 const express = require('express')
 const router = express.Router()
 
-const Logging = require('@google-cloud/logging')
-const loggingClient = Logging({
+const { Logging } = require('@google-cloud/logging')
+const logging = new Logging({
   projectId: config.GCP_PROJECT_ID,
-  keyFilename: config.GCP_KEYFILE,
+  keyFilename: config.GCP_KEYFILE
 })
 
 router.post('/', (req, res) => {
   debug('Got Logging call!(from ip', req.clientIp, ')')
   const data = Object.assign({}, req.body, { ip: req.clientIp, }) || {}
-  const log = loggingClient.log(config.GCP_STACKDRIVER_LOG_NAME)
+  const log = logging.log(config.GCP_STACKDRIVER_LOG_NAME)
   const metadata = { resource: { type: 'global', }, }
   const entry = log.entry(metadata, data)
 
-  log.write(entry).then(() => {
-    debug('Logging successfully.')
-  })
-  .catch(err => {
-    console.error('Client info logging error occurred:', err)
-  })
+  log.write(entry)
+    .then(() => debug('Logging successfully.'))
+    .catch(err => console.error('Client info logging error occurred:', err))
 
   /**
    * response whatever, it doesn't matter if logging in fail
